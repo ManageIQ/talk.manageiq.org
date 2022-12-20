4ec55a36ee492fdcf2105de2acc8346d7d01b2d5
Jason Frey <fryguy9@gmail.com>
2022-12-20 17:03:52 -0500

    Clear the interval when the counter elapses
    Without this change, the counter can go into the negative, which looks
    weird. More importantly, however, if the replace is too slow, then it
    will keep try to replace every second, which will flash the screen. No
    need to keep doing that once we hit 0.

--- redirect.js
+++ redirect.js
@@ -1,11 +1,12 @@
 const link = "https://web.archive.org/web/" + window.location.href;
 document.querySelector("#link").setAttribute('href', link);
 
-setInterval(function() {
+const interval = setInterval(function() {
 	let counter = document.querySelector("#counter");
 	let count = counter.textContent * 1 - 1;
 	counter.textContent = count;
 	if (count <= 0) {
+		clearInterval(interval);
 		window.location.replace(link);
 	}
 }, 1000);
